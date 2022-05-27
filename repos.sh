a801c90827ce26c0964eea733e98aa2aa449baad
root <root@example.com>
2022-05-27 18:55:58 +0200

    auto

--- repos.sh
+++ repos.sh
@@ -6,3 +6,13 @@ curl   -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH
 curl   -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}"   https://api.github.com/repos/j5pu/borrar/collaborators
 
 gh api
+f () { git submodule add "$@" && git add -A && git commit -q -m "auto" && git push -q; }
+fetchbranches() { git fetch -q --all; git branch -a; }
+
+repo="bats"
+branches="backup"
+url="https://github.com/mnopi/${repo}"
+f "${url}"
+for i in ${branches}; do
+  f --branch "${i}" "${url}" "branches/${repo}_${i}"
+done
