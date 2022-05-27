6b4a283ede2ce746dc9d18fd478fd0bc68c83d3a
root <root@example.com>
2022-05-27 16:18:58 +0200

    auto

---
+++ repos.sh
@@ -0,0 +1,8 @@
+
+mnopi
+curl   -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}"   https://api.github.com/repos/mnopi/mnopi/collaborators/j5pu
+curl   -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}"   https://api.github.com/repos/j5pu/borrar/collaborators/j5pu
+
+curl   -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}"   https://api.github.com/repos/j5pu/borrar/collaborators
+
+gh api
